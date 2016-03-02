2fa96340da3a9ae91458be050754546600489c2a
F. Hediger <hedigerf@gmail.com>
2016-03-02 22:11:14 +0100

    simple example :)

---
+++ App.jsx
@@ -0,0 +1,9 @@
+"user strict";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Counter2 from './Test.jsx';
+
+//var Counter = require('./Test.jsx');
+
+ReactDOM.render(<Counter2 />,  document.getElementById('example'));
---
+++ Test.jsx
@@ -0,0 +1,40 @@
+"user strict";
+import React from 'react';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: props.initialCount};
+  }
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
+  render() {
+    return (
+      <div onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
+    );
+  }
+}
+Counter.propTypes = { initialCount: React.PropTypes.number };
+Counter.defaultProps = { initialCount: 0 };
+
+export default class Counter2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: props.initialCount};
+  }
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
+  render() {
+    return (
+      <div onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
+    );
+  }
+}
+Counter2.propTypes = { initialCount: React.PropTypes.number };
+Counter2.defaultProps = { initialCount: 0 };
